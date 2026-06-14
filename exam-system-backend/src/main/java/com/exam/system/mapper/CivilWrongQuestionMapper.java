57df3f64d3b1e9b03a98c528c617113f28d74592
Yhuiwen <151928412+Yhuiwen@users.noreply.github.com>
2026-06-14 15:26:50 +0800

    add civil service wrong question mapper

---
+++ exam-system-backend/src/main/java/com/exam/system/mapper/CivilWrongQuestionMapper.java
@@ -0,0 +1,6 @@
+package com.exam.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.exam.system.entity.CivilWrongQuestion;
+
+public interface CivilWrongQuestionMapper extends BaseMapper<CivilWrongQuestion> {}
