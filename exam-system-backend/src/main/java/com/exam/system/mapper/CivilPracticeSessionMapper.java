ddec8cebf48b35f522552e76a86cb23c00eece9f
Yhuiwen <151928412+Yhuiwen@users.noreply.github.com>
2026-06-14 15:26:37 +0800

    add civil service practice session mapper

---
+++ exam-system-backend/src/main/java/com/exam/system/mapper/CivilPracticeSessionMapper.java
@@ -0,0 +1,6 @@
+package com.exam.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.exam.system.entity.CivilPracticeSession;
+
+public interface CivilPracticeSessionMapper extends BaseMapper<CivilPracticeSession> {}
