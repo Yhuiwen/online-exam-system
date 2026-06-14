c4834fe77e406d338e20f87adf5f0dbd2a99f39a
Yhuiwen <151928412+Yhuiwen@users.noreply.github.com>
2026-06-14 15:26:46 +0800

    add civil service practice answer mapper

---
+++ exam-system-backend/src/main/java/com/exam/system/mapper/CivilPracticeAnswerMapper.java
@@ -0,0 +1,6 @@
+package com.exam.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.exam.system.entity.CivilPracticeAnswer;
+
+public interface CivilPracticeAnswerMapper extends BaseMapper<CivilPracticeAnswer> {}
